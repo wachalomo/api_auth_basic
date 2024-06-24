c362eec80cc8be316d7f6dd16f94ee2212ade4a0
Christopher Arredondo <arredgroup@gmail.com>
2024-06-24 16:24:56 -0400

    added cors middleware

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import UserController from './controllers/UserController.js';
 import AuthController from './controllers/AuthController.js';
-
 const app = express();
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        // Allow follow-up middleware to override this CORS for options
+        preflightContinue: true,
+    }),
+);
 
 app.use(bodyParser.json());
 app.use('/api/v1/users', UserController);
 app.use('/api/v1/auth', AuthController);
 
-app.listen(3000, ()  => {
-    console.log('Server is running on port 3000');
+app.listen(3001, ()  => {
+    console.log('Server is running on port 3001');
 });
